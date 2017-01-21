2cf22a05eaecf6e0cf908ee25b57b5c13dae0f79
Guilherme Ceolin <guiceolin@gmail.com>
2017-01-20 23:26:39 -0200

    split studies

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'dist');
-var APP_DIR = path.resolve(__dirname, 'src');
-
 var config = {
-  entry: APP_DIR + '/app',
+  entry: {
+    1: '1.js',
+  },
   output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
+    path: path.resolve('./dist'),
+    filename: '[name].bundle.js'
   },
   module : {
     loaders : [
       {
         test : /\.(js|jsx)$/,
-        include : APP_DIR,
         loader : 'babel'
       }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
+    root: [
+      path.resolve('./src'),
+    ]
   }
 };
 
